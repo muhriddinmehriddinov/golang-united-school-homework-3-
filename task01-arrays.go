00d0a52e1acc2da5098e3ea936fe2e8f51dd48e4
Muhriddin <muhriddinmehriddinov01@gmail.com>
2022-06-28 00:00:44 +0500

    first

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,9 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	float32(j):=0
-for i:=0;i<len(input);i++{
-j:=j+input[i]
-}
-	return float32( j/len(input))
+	var j float32
+	for i := 0; i < len(input); i++ {
+		j = j + input[i]
+	}
+	return j / float32(len(input))
 }

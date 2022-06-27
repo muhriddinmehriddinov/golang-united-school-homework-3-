67981938526f6e8b2e4854232e84bce46120058c
Muhriddin <muhriddinmehriddinov01@gmail.com>
2022-06-27 22:43:24 +0500

    commit

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,12 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	for i:=0;i<len(input);i++{
+		slice2=input[1:len(input)+1]
+		for i:=0;i<len(input);i++{
+			input[i]=slice2[len(slice2)]
+			len(slice2)=len(slice2)-1
+		}
+	}
+	return input [] 
 }
